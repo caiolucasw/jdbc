282bbb8483073755e36502d2d8aa3be75b48893e
Caio Lucas <caiolsds@gmail.com>
2023-12-21 13:35:56 -0300

    Seller class added

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Department obj = new Department(1, "Books");
+		Department department = new Department(1, "Books");
+		Seller seller = new Seller(21, "Caio", "caio@gmail.com", new Date(), 4000.0, department);
 		
-		System.out.println(obj);
+		System.out.println(department);
+		System.out.println(seller);
 
 	}
 
